6534697e64596c4322a757599813e8edbc539c2b
AresNeutron <arexneutron@gmail.com>
2025-07-15 18:04:13 +0000

    retrying

--- main.py
+++ main.py
@@ -66,10 +66,10 @@ app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000/",
-        "https://kingslayer-frontend-fabio-quevedos-projects.vercel.app/",
-        "https://kingslayer-frontend.vercel.app/",
-        "https://kingslayer-frontend-git-main-fabio-quevedos-projects.vercel.app/",
+        "http://localhost:3000",
+        "https://kingslayer-frontend-fabio-quevedos-projects.vercel.app",
+        "https://kingslayer-frontend.vercel.app",
+        "https://kingslayer-frontend-git-main-fabio-quevedos-projects.vercel.app",
         "https://vercel.com/fabio-quevedos-projects/kingslayer-frontend/6MEisVT5ooqrUXjUbgExjkAeWBrA"
         ],
     allow_credentials=True,
